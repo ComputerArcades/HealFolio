94b39bdad3302aa3b2986e539d7ed9b510b791f4
unknown <tumbone@gmail.com>
2016-09-12 03:05:02 +0200

    Testing functionality to account type

--- Web-Application/healfolio-web-app/app/dashboardCtrl.js
+++ Web-Application/healfolio-web-app/app/dashboardCtrl.js
@@ -1,4 +1,6 @@
-app.controller('dashboardCtrl', function ($scope, $firebaseArray, $firebaseAuth, $rootScope, $filter,$routeParams, $location) {
+app.controller('dashboardCtrl', function ($scope, $firebaseArray, $firebaseObject, $firebaseAuth, $rootScope, $filter,$routeParams, $location) {
+
+    //This code below will go on the app's Routing function
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
             // User is signed in.
@@ -9,10 +11,39 @@ app.controller('dashboardCtrl', function ($scope, $firebaseArray, $firebaseAuth,
         }
     });
 
+    var user = firebase.auth().currentUser;
+    $scope.user_auth = {};
+
+    if(user){
+        var ref = firebase.database().ref("users").child(user.uid);
+
+        // return it as a synchronized object
+        $scope.user_auth = $firebaseObject(ref);
+//    $scope.user_auth.$loaded()
+//        .then(function(){
+//            console.log($scope.user_auth);
+//        })
+//        .catch(function(error){
+//            //Failure callback
+//            console.log(error);
+//        });
+    }else{
+        console.log("Error: Page was loaded outside the scope of the application!");
+    }
+
+
+
+
+
+
+
+
+
     $scope.doLogout = function(){
         firebase.auth().signOut();
         $location.path("/login");
-    }
+    };
+
 
 });
 
